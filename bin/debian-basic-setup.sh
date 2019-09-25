6530fdc59de1e123a63ef86855577096118a174b
Joao Lourenco <joao_lourenco@atlantico.com.br>
2019-09-25 16:24:11 -0300

    add basic setup for debian

---
+++ bin/debian-basic-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update
+apt-get install git -y
+apt-get install util-linux -y
+apt-get install htop -y
+apt-get install zsh -y
+apt-get install zip -y
+apt-get install curl -y
+apt-get install dkms -y
+apt-get install sudo -y
+apt-get install net-tools -y
+apt-get instlal openssh-server -y
+apt-get install apt-transport-https -y
+apt-get install vim -y
+apt-get install tmux -y
+apt-get install linux-headers-$(uname -r) -y
