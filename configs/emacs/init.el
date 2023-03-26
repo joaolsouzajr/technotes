c20b8cf6decbf6f7575a94f18f207f91309fde18
Joao L Souza Jr <lourencoccc@gmail.com>
2023-03-26 14:26:15 -0300

    add go and clojuere configs

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -1,3 +1,5 @@
+
+
 ;; You will most likely need to adjust this font size for your system!
 (defvar efs/default-font-size 180)
 (defvar efs/default-variable-font-size 180)
@@ -21,7 +23,8 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("elpa" . "https://elpa.gnu.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -53,10 +56,10 @@
 
 
 ;; Set frame transparency
-;;(set-frame-parameter (selected-frame) 'alpha efs/frame-transparency)
-;;(add-to-list 'default-frame-alist `(alpha . ,efs/frame-transparency))
-;;(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
-;;(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(set-frame-parameter (selected-frame) 'alpha efs/frame-transparency)
+(add-to-list 'default-frame-alist `(alpha . ,efs/frame-transparency))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
@@ -66,19 +69,35 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(set-face-attribute 'default nil :font "Fira Code Retina" :height efs/default-font-size)
+;;(set-face-attribute 'default nil :font "Fira Code Retina" :height efs/default-font-size)
 
 ;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height efs/default-font-size)
+;;(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height efs/default-font-size)
 
 ;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil :font "Cantarell" :height efs/default-variable-font-size :weight 'regular)
+;;(set-face-attribute 'variable-pitch nil :font "Cantarell" :height efs/default-variable-font-size :weight 'regular)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(set-face-attribute 'default nil :font "Fira Code retina" :height 280)
+;;(set-face-attribute 'default nil :font "Fira Code retina" :height 280)
 
-(load-theme 'wombat)
+;; (load-theme 'wombat)
+
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+
+(use-package exec-path-from-shell)
+
+
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;;(use-package yaml-mode)
+;;(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+
 
 (use-package ivy
   :diminish
@@ -98,12 +117,12 @@
   :config
   (ivy-mode 1))
 
-
 (use-package command-log-mode
   :commands command-log-mode)
 
 (use-package doom-themes
-  :init (load-theme 'doom-monokai-classic t))
+  ;; :init (load-theme 'doom-material-dark t))
+  :init (load-theme 'doom-Iosvkem t))
 
 (use-package all-the-icons
   :if (display-graphic-p))
@@ -145,13 +164,18 @@
   ([remap describe-key] . helpful-key))
 
 
-(use-package hydra)
-
 (defhydra hydra-text-scale (:timeout 4)
   "scale text"
   ("j" text-scale-increase "in")
   ("k" text-scale-decrease "out")
   ("f" nil "finished" :exit t))
+
+;(defhydra hydra-zoom (global-map "<f2>")
+;  "zoom"
+;  ("g" text-scale-increase "in")
+;  ("l" text-scale-decrease "out"))
+
+(use-package hydra)
 
 (use-package projectile
   :diminish projectile-mode
@@ -163,7 +187,9 @@
   ;; NOTE: Set this to the folder where you keep your Git repos!
   (when (file-directory-p "~/Labs")
     (setq projectile-project-search-path '("~/Labs")))
-  (setq projectile-switch-project-action #'projectile-dired))
+  (setq projectile-switch-project-action #'projectile-dired)
+  (setq projectile-globally-ignored-directories '("~/Labs/archlabs"))
+  (setq projectile-completion-system 'ivy))
 
 (use-package counsel-projectile
   :after projectile
@@ -182,14 +208,26 @@
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-headerline-breadcrumb-mode))
 
+
+;; flycheck syntax checker
+(use-package flycheck
+  :init (global-flycheck-mode))
+
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
-  :hook (lsp-mode . efs/lsp-mode-setup)
+  :hook ((lsp-mode . efs/lsp-mode-setup)
+	 (python-mode . lsp-deferred)
+         (go-mode . lsp-deferred)
+         (rust-mode . lsp-deferred)
+         (typescript-mode . lsp-deferred)
+	 (clojure-mode . lsp-deferred)
+	 (java-mode . lsp-deferred))
   :init
   (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
   :config
   (lsp-enable-which-key-integration t)
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
@@ -198,6 +236,32 @@
   (lsp-ui-peek-always-show t)
   (lsp-ui-sideline-show-hover t)
   (lsp-ui-doc-enable t))
+
+;; DAP
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+  :commands dap-debug
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+  (require 'dap-dlv-go)
+  (require 'dap-java)
+  (require 'dap-hydra)
+  (require 'dap-gdb-lldb)
+  (dap-gdb-lldb-setup)
+  ; (setq dap-lldb-debug-program '("/usr/local/Cellar/llvm/11.1.0/bin/lldb-vscode")))
+
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :wk "debugger")))
+
 
 ;; (use-package vertico
   ;; :ensure t
@@ -214,7 +278,7 @@
   ;; :config
   ;; (setq completion-styles '(orderless)))
 
-;; (use-package consult
+; (use-package consult
   ;; :ensure t)
 
 (use-package company
@@ -260,7 +324,7 @@
             ;; ("M-j" . lsp-ui-imenu)
             ;; ("M-?" . lsp-find-references)
 	    ;; ("C-c C-c l" . flycheck-list-errors)
-            ;; ("C-c C-c a" . lsp-execute-code-action)
+  ;; ("C-c C-c a" . lsp-execute-code-action)
             ;; ("C-c C-c r" . lsp-rename)
             ;; ("C-c C-c q" . lsp-workspace-restart)
             ;; ("C-c C-c Q" . lsp-workspace-shutdown)
@@ -279,4 +343,171 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;;(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+
+(use-package term
+  :config
+  (setq explicit-shell-file-name "zsh") ;; Change this to zsh, etc
+  ;;(setq explicit-zsh-args '())         ;; Use 'explicit-<shell>-args for shell-specific args
+
+  ;; Match the default Bash shell prompt.  Update this if you have a custom prompt
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
+
+
+;; (global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+
+
+;; (use-package projectile)
+(use-package yasnippet 
+  :hook (
+         (go-mode . yas-minor-mode)
+         (python-mode . yas-minor-mode)
+	 (clojure-mode . yas-minor-mode)
+	 (java-mode . yas-minor-mode)
+         )
+  :config (yas-global-mode))
+
+
+(setenv "JAVA_HOME"  "~/.sdkman/candidates/java/current/")
+(setq lsp-java-java-path "~/.sdkman/candidates/java/current/bin/java")
+
+(use-package lsp-java :ensure t :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-java :ensure nil)
+
+(use-package helm
+  :config (helm-mode))
+
+(use-package helm-lsp
+:ensure t
+:after (lsp-mode)
+:commands (helm-lsp-workspace-symbol)
+:init (define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol))
+
+
+(use-package go-mode :after lsp)
+
+(require 'lsp-mode)
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+(use-package go-tag :after go-mod)
+
+(with-eval-after-load 'go-mode
+  (define-key go-mode-map (kbd "C-c t") #'go-tag-add)
+  (define-key go-mode-map (kbd "C-c T") #'go-tag-remove))
+
+
+
+  ;; makes handling lisp expressions much, much easier
+  ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+(use-package paredit)
+
+ ;; key bindings and code colorization for Clojure
+ ;; https://github.com/clojure-emacs/clojure-mode
+ (use-package clojure-mode)
+ ;; extra syntax highlighting for clojure
+ (use-package clojure-mode-extra-font-locking)
+    
+ ;; integration with a Clojure REPL
+ ;; https://github.com/clojure-emacs/cider
+ (use-package cider)
+
+
+;;;;
+;; Clojure
+;;;;
+(require 'lsp-mode)
+(add-hook 'clojure-mode-hook #'lsp-deferred)
+
+;; Enable paredit for Clojure
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+;; This is useful for working with camel-case tokens, like names of
+;; Java classes (e.g. JavaClassName)
+(add-hook 'clojure-mode-hook 'subword-mode)
+
+;; A little more syntax highlighting
+(require 'clojure-mode-extra-font-locking)
+
+;; syntax hilighting for midje
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (setq inferior-lisp-program "lein repl")
+            (font-lock-add-keywords
+             nil
+             '(("(\\(facts?\\)"
+                (1 font-lock-keyword-face))
+               ("(\\(background?\\)"
+                (1 font-lock-keyword-face))))
+            (define-clojure-indent (fact 1))
+            (define-clojure-indent (facts 1))
+            (rainbow-delimiters-mode)))
+
+;;;;
+;; Cider
+;;;;
+
+;; provides minibuffer documentation for the code you're typing into the repl
+(add-hook 'cider-mode-hook 'eldoc-mode)
+
+;; go right to the REPL buffer when it's finished connecting
+(setq cider-repl-pop-to-buffer-on-connect t)
+
+;; When there's a cider error, show its buffer and switch to it
+(setq cider-show-error-buffer t)
+(setq cider-auto-select-error-buffer t)
+
+;; Where to store the cider history.
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+;; Wrap when navigating history.
+(setq cider-repl-wrap-history t)
+
+;; enable paredit in your REPL
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;; Use clojure mode for other extensions
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
+
+
+;; key bindings
+;; these help me out with the way I usually develop web apps
+(defun cider-start-http-server ()
+  (interactive)
+  (cider-load-current-buffer)
+  (let ((ns (cider-current-ns)))
+    (cider-repl-set-ns ns)
+    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
+    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
+
+
+(defun cider-refresh ()
+  (interactive)
+  (cider-interactive-eval (format "(user/reset)")))
+
+(defun cider-user-ns ()
+  (interactive)
+  (cider-repl-set-ns "user"))
+
+(eval-after-load 'cider
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
+     (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
+     (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
+     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+
